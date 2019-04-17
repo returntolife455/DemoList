843ced436016cf9c824c48e261cb0ea904e536a6
chenwangjun <cwj123456>
2019-04-17 11:02:49 +0800

    test push 0417

--- app/src/main/java/com/returntolife/jjcode/mydemolist/AppApplication.java
+++ app/src/main/java/com/returntolife/jjcode/mydemolist/AppApplication.java
@@ -3,11 +3,6 @@ package com.returntolife.jjcode.mydemolist;
 import android.app.Application;
 import android.content.Context;
 
-import com.tools.jj.tools.GlideLoader;
-import com.tools.jj.tools.http.Http;
-import com.tools.jj.tools.imageload.ImageLoader;
-import com.tools.jj.tools.utils.LogUtil;
-
 /**
  * Created by HeJiaJun on 2019/4/17.
  * des:
@@ -15,7 +10,7 @@ import com.tools.jj.tools.utils.LogUtil;
  */
 public class AppApplication extends Application {
 
-
+    private static final String TAG = "AppApplication";
     public static Context pAppContext=null;
 
     @Override
