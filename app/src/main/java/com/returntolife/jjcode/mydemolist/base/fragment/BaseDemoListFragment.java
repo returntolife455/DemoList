dae4091f75795a8f6199ea247eaf476ce0910661
jj <1021661582@qq.com>
2019-06-09 14:10:43 +0800

    修改主页item布局样式

--- app/src/main/java/com/returntolife/jjcode/mydemolist/base/fragment/BaseDemoListFragment.java
+++ app/src/main/java/com/returntolife/jjcode/mydemolist/base/fragment/BaseDemoListFragment.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,40 +41,18 @@ public abstract  class BaseDemoListFragment extends BaseDemoFragment {
         data=initData();
 
         mRecyvlerView=rootView.findViewById(R.id.rv_main);
-        //将item以及空隙均分
-        int screenWidth= Dp2PxUtil.getScreenWidth((Activity) context);
-        int viewWidth=screenWidth/2;
-        int space=viewWidth- (int) (context.getResources().getDimension(R.dimen.app_demolist_fragment_item_width));
-        final int diff=space*2/3;
-        GridLayoutManager manager=new GridLayoutManager(context,2);
-        mRecyvlerView.setLayoutManager(manager);
-        if (mRecyvlerView.getItemDecorationCount() == 0) {
-            mRecyvlerView.addItemDecoration(new RecyclerView.ItemDecoration() {
-                @Override
-                public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-                    if(parent.getChildAdapterPosition(view)%2==0){
-                        outRect.left=diff;
-                        outRect.right=diff/2;
-                    }else {
-                        outRect.left=diff/2;
-                        outRect.right=diff;
-                    }
-
-                    outRect.top=diff;
-                }
-            });
-        }
+        initRecyclerViewLayout();
         mRecyvlerView.setAdapter(new CommonRecyclerAdapter<DemoListBean>(context,R.layout.item_demolist_fragment,data) {
             @Override
             public void convert(BaseRecyclerViewHolder holder, final DemoListBean demoListBean, int position) {
                 holder.setText(R.id.tv_title,demoListBean.getTitle());
                 holder.setText(R.id.tv_des,demoListBean.getDes());
                 holder.setText(R.id.tv_time,demoListBean.getTime());
-                if(demoListBean.getImageResource()!=0){
-                    holder.setImageResource(R.id.iv_item_bg,demoListBean.getImageResource());
-                }else if(!StringUtil.isEmpty(demoListBean.getImageUrl())){
-                    holder.setImageUrl(R.id.iv_item_bg,demoListBean.getImageUrl());
-                }
+//                if(demoListBean.getImageResource()!=0){
+//                    holder.setImageResource(R.id.iv_item_bg,demoListBean.getImageResource());
+//                }else if(!StringUtil.isEmpty(demoListBean.getImageUrl())){
+//                    holder.setImageUrl(R.id.iv_item_bg,demoListBean.getImageUrl());
+//                }
                 holder.setOnClickListener(R.id.cardview_item, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -82,6 +61,48 @@ public abstract  class BaseDemoListFragment extends BaseDemoFragment {
                 });
             }
         });
+    }
+
+    private void initRecyclerViewLayout() {
+//        //将item以及空隙均分
+//        int screenWidth= Dp2PxUtil.getScreenWidth((Activity) context);
+//        int viewWidth=screenWidth/2;
+//        int space=viewWidth- (int) (context.getResources().getDimension(R.dimen.app_demolist_fragment_item_width));
+//        final int diff=space*2/3;
+//        GridLayoutManager manager=new GridLayoutManager(context,2);
+//        mRecyvlerView.setLayoutManager(manager);
+//        if (mRecyvlerView.getItemDecorationCount() == 0) {
+//            mRecyvlerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+//                @Override
+//                public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+//                    if(parent.getChildAdapterPosition(view)%2==0){
+//                        outRect.left=diff;
+//                        outRect.right=diff/2;
+//                    }else {
+//                        outRect.left=diff/2;
+//                        outRect.right=diff;
+//                    }
+//
+//                    outRect.top=diff;
+//                }
+//            });
+//        }
+        mRecyvlerView.setLayoutManager(new LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false));
+        if(mRecyvlerView.getItemDecorationCount()==0){
+            mRecyvlerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+                @Override
+                public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                    outRect.left=10;
+                    outRect.right=10;
+                    outRect.top=15;
+                    outRect.bottom=15;
+                    if(parent.getChildAdapterPosition(view)==0){
+                        outRect.top=30;
+                    }
+
+                }
+            });
+        }
     }
 
 
