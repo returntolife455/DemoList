335830b0bf825da7d8ee4c8c010a3865a2e0bf3b
hejiajun <1021661582@qq.com>
2021-05-28 15:39:50 +0800

    feat:增加一个轮询封装

--- app/src/main/java/com/returntolife/jjcode/mydemolist/demo/function/coroutine/CoroutineTest1.kt
+++ app/src/main/java/com/returntolife/jjcode/mydemolist/demo/function/coroutine/CoroutineTest1.kt
@@ -4,33 +4,61 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import com.tools.jj.tools.utils.LogUtil
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import org.json.JSONObject
 
 /**
  * Created by HeJiaJun on 2020/7/31.
  * Email:hejj@mama.cn
  * des:
  */
-class CoroutineTest1:Activity() {
+class CoroutineTest1:Activity(), CoroutineScope by MainScope() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-        var job = GlobalScope.launch(Dispatchers.Main) {
-            var content = fetchData()
-            LogUtil.d(content)
+        launch(Dispatchers.Main) {
+            LogUtil.d(Thread.currentThread().name)
+            try {
+                val result1 = async { fetchData() }
+                val result2 = async { fetchData2("haha")}
+                LogUtil.d(result1.await()+result2.await())
+
+            } catch (e: Exception) {
+                LogUtil.d(e.toString())
+
+            }
+
+
         }
-        LogUtil.d("coroutine after")
+
+        LogUtil.d("GlobalScope.launch after")
     }
 
 
     suspend fun fetchData():String{
-        delay(2000)
+        withContext(Dispatchers.IO) {
+            LogUtil.d(Thread.currentThread().name)
+            delay(2000)
+            LogUtil.d("fetchData end")
+        }
         return "content"
+
+    }
+
+    suspend fun fetchData2(content:String):String{
+        withContext(Dispatchers.IO) {
+            LogUtil.d(Thread.currentThread().name)
+            delay(3000)
+            LogUtil.d("fetchData2 end")
+        }
+        return "fetchData2--$content"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
     }
 }
---
+++ toollibrary/src/main/java/com/tools/jj/tools/utils/Trigger.java
@@ -0,0 +1,109 @@
+package com.tools.jj.tools.utils;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+
+import static io.reactivex.internal.disposables.DisposableHelper.DISPOSED;
+
+/**
+ * @Author : hejiajun
+ * @Time : 2021/5/25
+ * @Email : hejiajun@lizhi.fm
+ * @Desc :
+ */
+public class Trigger {
+
+    public boolean mRepeat;
+
+    private final TriggerExecutor mExecutor;
+
+    private Disposable mDisposable = DISPOSED;
+
+    Scheduler mScheduler = null;
+    /**
+     * 默认执行再主线程
+     *
+     * @param exe     TriggerExecutor
+     * @param isConti mRepeat
+     */
+    public Trigger(TriggerExecutor exe, boolean isConti) {
+        this(exe, isConti, true);
+    }
+
+    /**
+     * 是否执行主线程
+     *
+     * @param exe     TriggerExecutor
+     * @param isConti mRepeat
+     */
+    public Trigger(TriggerExecutor exe, boolean isConti, boolean isMainThread) {
+        mExecutor = exe;
+        mRepeat = isConti;
+        mScheduler = isMainThread ? AndroidSchedulers.mainThread() : Schedulers.single();
+    }
+
+    public final void cancel() {
+        if (!mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
+    }
+
+    /**
+     * 设置超时检查的时间.
+     *
+     * @param time 超时时间
+     */
+    public void delayed(long time) {
+        cancel();
+        Observable<Long> observable = mRepeat ?  Observable.interval(time, time, TimeUnit.MILLISECONDS)
+                : Observable.timer(time, TimeUnit.MILLISECONDS);
+
+        mDisposable = observable .subscribeOn(Schedulers.io())
+                .observeOn(mScheduler)
+                .map(new Function<Long, Boolean>() {
+                    @Override
+                    public Boolean apply(@NonNull Long aLong) throws Exception {
+                        return mExecutor.execute();
+                    }
+                })
+                //捕捉运行时异常，不退出线程
+                .onErrorReturn(new Function<Throwable, Boolean>() {
+                    @Override
+                    public Boolean apply(@NonNull Throwable throwable) throws Exception {
+                        LogUtil.e( "Trigger.execute error! ="+throwable);
+                        return false;
+                    }
+                })
+                //返回false时，停止定时任务
+                .takeUntil(new Predicate<Boolean>() {
+                    @Override
+                    public boolean test(@NonNull Boolean aBoolean) throws Exception {
+                        return !aBoolean;
+                    }
+                })
+                .subscribe();
+    }
+
+    public boolean b() {
+        return mDisposable.isDisposed();
+    }
+
+    @Override
+    protected void finalize() {
+        cancel();
+    }
+
+    public interface TriggerExecutor {
+        boolean execute();
+    }
+}
